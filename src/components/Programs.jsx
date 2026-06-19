fab36ce2a81b5cf07e0b3e51eeeec79625ae579f
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-19 14:43:04 +0300

    Fix App.css layout restrictions

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -14,7 +14,7 @@ import {
 import "./Programs.css";
 
 import nurseryImg from "../assets/nursery.jpg";
-import primaryImg from "../assets/primary.jpg";
+import primaryImg from "../assets/p.jpg";
 
 function Programs() {
   return (
