1e6acebc34bc8057feb52ea7d40bd1760b49229d
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-19 12:31:41 +0300

    Fix App.css layout restrictions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import {
   FaPhoneAlt,
@@ -5,19 +6,20 @@ import {
   FaMapMarkerAlt,
   FaArrowRight,
   FaBars,
+  FaTimes,
 } from "react-icons/fa";
 
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {/* TOP BAR */}
       <div className="topbar">
-
         <div className="topbar-left">
-
           <span>
             <FaPhoneAlt />
             0744 523 317
@@ -27,48 +29,32 @@ function Navbar() {
             <FaEnvelope />
             info@praiseel.ac.ug
           </span>
-
         </div>
 
         <div className="topbar-right">
-
           <span>
             <FaMapMarkerAlt />
             Kokoola-Kalagi, Mukono
           </span>
-
         </div>
-
       </div>
 
       {/* NAVBAR */}
       <header className="navbar">
-
-        <Link
-          to="/"
-          className="logo-section"
-        >
+        <Link to="/" className="logo-section">
           <img
             src={logo}
             alt="Praise-El Junior School"
           />
 
           <div className="school-info">
-
-            <h2>
-              Praise-El Junior School
-            </h2>
-
-            <span>
-              Aspiring For Excellence
-            </span>
-
+            <h2>Praise-El Junior School</h2>
+            <span>Aspiring For Excellence</span>
           </div>
-
         </Link>
 
+        {/* DESKTOP MENU */}
         <nav className="nav-menu">
-
           <NavLink
             to="/"
             end
@@ -123,7 +109,6 @@ function Navbar() {
           >
             Apply Online
           </NavLink>
-
         </nav>
 
         <Link
@@ -137,11 +122,91 @@ function Navbar() {
           </div>
         </Link>
 
-        <div className="mobile-menu">
-          <FaBars />
+        {/* MOBILE MENU ICON */}
+        <div
+          className="mobile-menu"
+          onClick={() =>
+            setMenuOpen(!menuOpen)
+          }
+        >
+          {menuOpen ? (
+            <FaTimes />
+          ) : (
+            <FaBars />
+          )}
         </div>
-
       </header>
+
+      {/* MOBILE NAVIGATION */}
+      {menuOpen && (
+        <div className="mobile-nav">
+
+          <NavLink
+            to="/"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            About Us
+          </NavLink>
+
+          <NavLink
+            to="/admissions"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Admissions
+          </NavLink>
+
+          <NavLink
+            to="/gallery"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Gallery
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Contact Us
+          </NavLink>
+
+          <NavLink
+            to="/apply-online"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Apply Online
+          </NavLink>
+
+          <Link
+            to="/admissions"
+            className="mobile-enroll-btn"
+            onClick={() =>
+              setMenuOpen(false)
+            }
+          >
+            Enroll Now
+          </Link>
+
+        </div>
+      )}
     </>
   );
 }
