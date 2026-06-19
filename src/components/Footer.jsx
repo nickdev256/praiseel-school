bf18fe957d272bc6c7c29f9cfa66efb277e49e49
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-19 14:57:39 +0300

    Fix App.css layout restrictions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
+
 import logo from "../assets/logo.png";
 import footerBg from "../assets/footer-bg.jpg";
 
@@ -8,7 +10,12 @@ import {
   FaFacebookF,
   FaInstagram,
   FaWhatsapp,
-  FaEnvelope
+  FaEnvelope,
+  FaGraduationCap,
+  FaShieldAlt,
+  FaUsers,
+  FaHeart,
+  FaPaperPlane,
 } from "react-icons/fa";
 
 function Footer() {
@@ -16,95 +23,142 @@ function Footer() {
     <footer
       className="footer"
       style={{
-        backgroundImage: `url(${footerBg})`
+        backgroundImage: `url(${footerBg})`,
       }}
     >
       <div className="footer-overlay">
 
-        <div className="footer-container">
+        {/* TOP LOGO */}
+
+        <div className="footer-top-logo">
+          <img src={logo} alt="Praise-El Junior School" />
+        </div>
+
+        {/* MAIN CONTENT */}
+
+        <div className="footer-main">
+
+          {/* BRAND */}
 
           <div className="footer-brand">
 
-            <img
-              src={logo}
-              alt="Praise-El Junior School"
-            />
+            <img src={logo} alt="Logo" />
 
             <h2>
-              Praise-El Junior School
+              Praise-El
+              <br />
+              Junior School
             </h2>
 
-            <p className="motto">
+            <span className="footer-motto">
               Aspiring For Excellence
-            </p>
+            </span>
 
             <p>
-              Nurturing young minds through quality
-              education, discipline, leadership and
-              character development.
+              Nurturing young minds through
+              quality education, discipline,
+              leadership and character
+              development.
             </p>
 
+            <div className="footer-values">
+
+              <div>
+                <FaGraduationCap />
+                <span>Quality Education</span>
+              </div>
+
+              <div>
+                <FaShieldAlt />
+                <span>Strong Values</span>
+              </div>
+
+              <div>
+                <FaUsers />
+                <span>Bright Futures</span>
+              </div>
+
+            </div>
+
           </div>
+
+          {/* LINKS */}
 
           <div className="footer-links">
 
             <h3>Quick Links</h3>
 
-            <a href="/">Home</a>
-            <a href="/about">About Us</a>
-            <a href="/academics">Academics</a>
-            <a href="/gallery">Gallery</a>
-            <a href="/contact">Contact</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/academics">Academics</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/admissions">Admissions</Link>
+            <Link to="/news">News & Events</Link>
+            <Link to="/contact">Contact Us</Link>
 
           </div>
+
+          {/* CONTACT */}
 
           <div className="footer-contact">
 
             <h3>Contact Us</h3>
 
-            <p>
+            <a href="tel:0744523317">
               <FaPhoneAlt />
-              0744523317
-            </p>
+              0744 523 317
+            </a>
 
-            <p>
+            <a href="tel:0786971859">
               <FaPhoneAlt />
-              0786971859
-            </p>
+              0786 971 859
+            </a>
 
-            <p>
+            <a href="tel:0756070601">
               <FaPhoneAlt />
-              0756070601
-            </p>
+              0756 070 601
+            </a>
 
-            <p>
+            <a href="tel:0758028596">
               <FaPhoneAlt />
-              0758028596
-            </p>
+              0758 028 596
+            </a>
 
-            <p>
+            <a href="tel:0752446049">
               <FaPhoneAlt />
-              0752446049
-            </p>
+              0752 446 049
+            </a>
 
-            <p>
+            <a href="mailto:info@praiseel.ac.ug">
               <FaEnvelope />
               info@praiseel.ac.ug
-            </p>
+            </a>
 
           </div>
 
+          {/* LOCATION */}
+
           <div className="footer-location">
 
-            <h3>Location</h3>
+            <h3>Our Location</h3>
 
-            <p>
+            <div className="location-box">
+
               <FaMapMarkerAlt />
-              Kokoola-Kalagi,
-              Along Gayaza Road,
-              Mukono District,
-              Uganda
-            </p>
+
+              <p>
+                Kokoola-Kalagi,
+                <br />
+                Along Gayaza Road,
+                <br />
+                Mukono District,
+                <br />
+                Uganda
+              </p>
+
+            </div>
+
+            <h4>Follow Us</h4>
 
             <div className="socials">
 
@@ -126,15 +180,69 @@ function Footer() {
 
         </div>
 
+        {/* NEWSLETTER */}
+
+        <div className="newsletter">
+
+          <div className="newsletter-left">
+
+            <div className="newsletter-icon">
+              <FaEnvelope />
+            </div>
+
+            <div>
+              <h4>Stay Connected</h4>
+
+              <p>
+                Subscribe for school updates
+                and events.
+              </p>
+            </div>
+
+          </div>
+
+          <form className="newsletter-form">
+
+            <input
+              type="email"
+              placeholder="Enter your email address"
+            />
+
+            <button type="submit">
+              Subscribe
+              <FaPaperPlane />
+            </button>
+
+          </form>
+
+          <div className="newsletter-quote">
+            “Every child is unique.
+            Every dream is important.”
+          </div>
+
+        </div>
+
+        {/* BOTTOM */}
+
         <div className="footer-bottom">
 
-          © 2026 Praise-El Junior School.
-          All Rights Reserved.
+          <p>
+            © 2026 Praise-El Junior School.
+            All Rights Reserved.
+          </p>
+
+          <div className="footer-heart">
+            <FaHeart />
+          </div>
+
+          <p>
+            Designed by
+            <span> Eth Tech Solutions</span>
+          </p>
 
         </div>
 
       </div>
-
     </footer>
   );
 }
