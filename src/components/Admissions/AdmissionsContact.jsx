d81a4fa94e1dc5e4b008c70e20cea33d0719356d
Nicholas Ssentongo <nickclas256@gmail.com>
2026-07-07 03:18:00 +0300

    resolve merge conflicts - keep local admissions components

--- src/components/Admissions/AdmissionsContact.jsx
+++ src/components/Admissions/AdmissionsContact.jsx
@@ -1,12 +1,8 @@
-
-import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
-
 import {
   FaPhoneAlt,
   FaMapMarkerAlt,
   FaEnvelope
 } from "react-icons/fa";
-
 
 import "./AdmissionsContact.css";
 
@@ -21,17 +17,28 @@ function AdmissionsContact() {
 
       <div className="contact-grid">
 
-
         {/* Phone */}
         <div className="contact-item">
           <FaPhoneAlt className="icon" />
-          <p><a href="tel:0744523317">0744523317</a></p>
-          <p><a href="tel:0786971859">0786971859</a></p>
+
+          <p>
+            <a href="tel:0744523317">
+              0744523317
+            </a>
+          </p>
+
+          <p>
+            <a href="tel:0786971859">
+              0786971859
+            </a>
+          </p>
         </div>
+
 
         {/* Email */}
         <div className="contact-item">
           <FaEnvelope className="icon" />
+
           <p>
             <a href="mailto:info@praiseel.ac.ug">
               info@praiseel.ac.ug
@@ -39,32 +46,17 @@ function AdmissionsContact() {
           </p>
         </div>
 
+
         {/* Location */}
         <div className="contact-item">
           <FaMapMarkerAlt className="icon" />
-          <p>Kokoola–Kalagi, Mukono District</p>
-        </div>
 
-
-        <div>
-          <FaPhoneAlt />
-          <p>0744523317</p>
-          <p>0786971859</p>
-        </div>
-
-        <div>
-          <FaEnvelope />
-          <p>info@praiseel.ac.ug</p>
-        </div>
-
-        <div>
-          <FaMapMarkerAlt />
           <p>
-            Kokoola-Kalagi,
+            Kokoola–Kalagi,
+            <br />
             Mukono District
           </p>
         </div>
-
 
       </div>
 
